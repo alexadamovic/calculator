a4ad6e141588613c5e5b60686dc89e4d012f9c52
Alex Adamovic <alexanderadamovic@gmail.com>
2022-01-12 14:20:09 -0800

    add colored buttons and multiple mathematical functions
    Co-authored-by: Matt Eilar <meilar@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business logic
+
 function add(number1, number2) {
   return number1 + number2;
 }
@@ -14,13 +16,51 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
+// user interface logic
+
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("button#btn-add").click(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+    const number1 = parseInt($("#user-number1").val());
+    const number2 = parseInt($("#user-number2").val());
     const result = add(number1, number2);
     console.log(result);
     $("#output").text(result);
+  });
+
+  $("button#btn-subtract").click(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#user-number1").val());
+    const number2 = parseInt($("#user-number2").val());
+    const result = subtract(number1, number2);
+    console.log(result);
+    $("#output").text(result); 
+  });
+
+  $("button#btn-multiply").click(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#user-number1").val());
+    const number2 = parseInt($("#user-number2").val());
+    const result = multiply(number1, number2);
+    console.log(result);
+    $("#output").text(result); 
+  });
+
+  $("button#btn-divide").click(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#user-number1").val());
+    const number2 = parseInt($("#user-number2").val());
+    const result = divide(number1, number2);
+    console.log(result);
+    $("#output").text(result); 
+  });
+
+  $("button#btn-clear").click(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#user-number1").val());
+    const number2 = parseInt($("#user-number2").val());
+    const result = add(number1, number2);
+    console.log(result);
+    $("#output").text(result); 
   });
 });
